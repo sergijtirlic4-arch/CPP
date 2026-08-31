a3049dc17f8a5ed68b4694bf0916bffa3a23614f
sergijtirlic4-arch <sergijtirlic4@gmail.com>
2026-08-31 18:30:26 +0200

    Add project files.

---
+++ 1_intro_to_C++/01_first_lesson.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "I\n";
+    cout << "\tLove\n";
+    cout << "\t\tYou\n";
+    cout << "\t\t\tC++!\n";
+
+    cout << "\t\tRESUME\n";
+    cout << "Name:\t\t\tSerhii\n";
+    cout << "Surname:\t\tTyrlych\n";
+    cout << "Last name:\t\tSerhiiovych\n\n";
+    cout << "Date of birth:\t\t02.05.11\n";
+    cout << "City:\t\t\tUzgorod\n";
+    cout << "Age:\t\t\t15\n";
+    cout << "Hobby:\t\t\tC++\n";
+
+    cout << "Every\n";
+    cout << "\thunter\n";
+    cout << "\t\twants\n";
+    cout << "\t\t\tto\n";
+    cout << "\t\t\t\tknow\n";
+    cout << "\t\t\t\t\twhere\n";
+    cout << "\t\t\t\t\t\tthe\n";
+    cout << "\t\t\t\t\t\t\tpheasant\n";
+    cout << "\t\t\t\t\t\t\t\tis.\n";
+
+
+    return 0;
+}
